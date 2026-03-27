4bed3ab1dbbdf50885b3e0361350749cda7936b5
Navnith Bharadwaj <bharadwajnavnith5@gmail.com>
2026-03-26 18:49:18 -0600

    updated worker version to 0.3.3

--- workers/python-worker/meshml_worker/cli.py
+++ workers/python-worker/meshml_worker/cli.py
@@ -32,7 +32,7 @@ def _default_config_path() -> Path:
 
 
 @click.group()
-@click.version_option(version="0.3.2")
+@click.version_option(version="0.3.3")
 def main() -> None:
     """MeshML Worker - Federated Learning Worker"""
     pass
